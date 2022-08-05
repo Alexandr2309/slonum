f4bf6672fdc145fe24e18db2194b67f6c6602399
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-06 08:09:45 +1000

    scroll

--- src/components/Footer/FooterScrollingTop.tsx
+++ src/components/Footer/FooterScrollingTop.tsx
@@ -3,8 +3,12 @@ import path from '../../helpers/imgPass'
 import cl from '../../styles/Footer.module.scss'
 
 function FooterScrollButton() {
+  const scrollToBegin = () => {
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
+  }
+
   return (
-    <div className={cl.scroll}>
+    <div className={cl.scroll} onClick={scrollToBegin}>
       <img src={path.arrow} alt="arrow" />
     </div>
   )
